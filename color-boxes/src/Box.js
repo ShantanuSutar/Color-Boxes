7e70f058996b57ffa228af1f412c88306289d41d
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-11-27 00:55:25 +0530

    FlexBox
    multiple boxes with random colors

--- color-boxes/src/Box.js
+++ color-boxes/src/Box.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import { choice } from './helpers';
 import './Box.css';
 class Box extends Component {
-    static defaultProps = {
-        allColors: ['purple', 'magenta', 'blue', 'teal']
-    }
+
     constructor(props) {
         super(props);
-        this.state = { color: choice(this.props.allColors) };
+        this.state = { color: choice(this.props.colors) };
         this.handleClick = this.handleClick.bind(this);
     }
     pickColor() {
         let newColor;
         do {
-            newColor = choice(this.props.allColors);
+            newColor = choice(this.props.colors);
         } while (newColor === this.state.color)
 
         this.setState({ color: newColor });
--- color-boxes/src/BoxContainer.js
+++ color-boxes/src/BoxContainer.js
@@ -1,12 +1,16 @@
 import { Component } from "react";
 import Box from "./Box";
+import './BoxContainer.css'
 
 class BoxContainer extends Component {
     static defaultProps = {
-        numBoxes: 18
+        numBoxes: 16,
+        allColors: ['purple', 'magenta', 'violet', 'pink', 'teal', 'aqua', 'coral', 'yellowgreen', 'turquoise', '	springgreen', 'powderblue', '	olive', 'palevioletred', 'maroon', 'lightsalmon', 'beige', 'bisque', 'aquamarine', 'indianred']
     }
     render() {
-        const boxes = Array.from({ length: this.props.numBoxes }).map(() => (<Box />));
+        const boxes = Array.from({ length: this.props.numBoxes }).map(() => (
+            <Box colors={this.props.allColors} />
+        ));
 
         return <div className="BoxContainer">
             {boxes}
